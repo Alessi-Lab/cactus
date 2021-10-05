b3c041def265fae0f495682771e128f54a08eb7e
Toan Phung <toan.phungkhoiquoctoan@gmail.com>
2021-10-05 11:32:18 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,10 +9,18 @@ class MainHandler(RequestHandler):
     def get(self):
         self.write("Hello, world")
 
+
+settings = {
+    "cookie_secret": "IVM/jkpE+1A4We2P/hVxkHe8EW8mW3TR574hEpCnuGrU3H5trJCSckecA9e2zYthBbI=",
+    "xsrf_cookies": True,
+}
+
+
 if __name__ == "__main__":
     if sys.platform.startswith("win32"):
         asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-    app = Application([
+    app = Application()
+    app.add_handlers(r'(localhost|127\.0\.0\.1|62.75.251.157/)', [
         (r"/", MainHandler)
     ])
     app.listen(80)
