20b150df459b0511fce25f00146084a7ec0ce8c6
Toan Phung <toan.phungkhoiquoctoan@gmail.com>
2021-10-06 20:48:44 +0100

    update

--- main.py
+++ main.py
@@ -11,12 +11,11 @@ from cactus.handlers import MainHandler, UniprotHandler
 define("port", default=8000, help="Port number")
 
 settings = {
-    "static_path": os.path.join(os.path.dirname(__file__), "static"),
-    "x-header": True
-    #"cookie_secret": "IVM/jkpE+1A4We2P/hVxkHe8EW8mW3TR574hEpCnuGrU3H5trJCSckecA9e2zYthBbI=",
-    #"xsrf_cookies": True,
+    # "static_path": os.path.join(os.path.dirname(__file__), "static"),
+    #"x-header": True
+    # "cookie_secret": "IVM/jkpE+1A4We2P/hVxkHe8EW8mW3TR574hEpCnuGrU3H5trJCSckecA9e2zYthBbI=",
+    # "xsrf_cookies": True,
 }
-
 
 if __name__ == "__main__":
     if sys.platform.startswith("win32"):
@@ -25,9 +24,9 @@ if __name__ == "__main__":
     app.add_handlers(
         r'(localhost|127\.0\.0\.1|62\.75\.251\.157|curtain\.proteo\.info)',
         [
-        (r"/", MainHandler),
-        (r"/uniprot", UniprotHandler),
-        #(r"/static", StaticFileHandler, dict(path=settings['static_path']))
+            (r"/", MainHandler),
+            (r"/uniprot", UniprotHandler),
+            # (r"/static", StaticFileHandler, dict(path=settings['static_path']))
         ])
     server = tornado.httpserver.HTTPServer(app)
     server.bind(options.port)
