7029b618335126230dd41f1ef44cbb3980f346ab
Toan Phung <toan.phungkhoiquoctoan@gmail.com>
2022-03-30 14:44:15 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-if [ "$1" = "1" ]; then \
+echo $1
+if [ $1 = "1" ]; then \
     cd /app/netphos/ && \
     uncompress /app/netphos/netphos-3.1.Linux.tar.Z && \
     tar -xvf /app/netphos/netphos-3.1.Linux.tar; \
