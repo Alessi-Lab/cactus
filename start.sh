63e4b777de7d951f7c31122db07cf1675b9a9aaf
Toan Phung <toan.phungkhoiquoctoan@gmail.com>
2022-03-30 15:29:11 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 if [ "${NETPHOS}" = "1" ]; then
+    cd /app/netphos;
     uncompress /app/netphos/netphos-3.1.Linux.tar.Z;
     tar -xvf /app/netphos/netphos-3.1.Linux.tar;
-    rm /app/netphos/ape-1.0;
+    rm /app/netphos/ape-1.0/ape;
     mv /app/cactus/ape.docker /app/netphos/ape-1.0/ape
 fi
-
+export NETPHOS="0"
 service nginx restart
 nginx -g "daemon off;"
 supervisord -n -c /app/cactus/super.docker.conf
