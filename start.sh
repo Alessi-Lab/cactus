d3705269144498765f8393c3dca4be7de7136800
Toan Phung <toan.phungkhoiquoctoan@gmail.com>
2022-03-30 14:33:47 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-
+if [ "$1" = "1" ]; then \
+    cd /app/netphos/ && \
+    uncompress /app/netphos/netphos-3.1.Linux.tar.Z && \
+    tar -xvf /app/netphos/netphos-3.1.Linux.tar; \
+fi
 service nginx restart
 nginx -g "daemon off;"
 supervisord -n -c /app/cactus/super.docker.conf
